c5fb60c4f5f2857558f9aa4d1c645966f50d5a80
ksuares1 <kimberlyjsuares@gmail.com>
2023-05-11 00:25:27 -0400

    added humidty and wind metrics

--- assets/css/js/app.js
+++ assets/css/js/app.js
@@ -58,11 +58,14 @@ searchForm.addEventListener("submit", search);
 
 
 function showWeather(response) {
-  console.log(response.data);
   let temperature = Math.round(response.data.main.temp);
   console.log(temperature);
   let currentTemperature = document.querySelector(".temperature");
-  currentTemperature.innerHTML = `${temperature}`;
+  currentTemperature.innerHTML=`${temperature}`;
+  let humidityElement=document.querySelector(".humidity");
+  humidityElement.innerHTML=response.data.main.humidity;
+  let windElement = document.querySelector(".wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
   let descriptionElement=document.querySelector(".description");
   descriptionElement.innerHTML=response.data.weather[0].description;
   let iconElement=document.querySelector("#icon");
