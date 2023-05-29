c832a18014b72bbabf6b42d7066449841741bfba
ksuares1 <kimberlyjsuares@gmail.com>
2023-05-28 23:38:45 -0400

    Added the days of the week using js

--- assets/css/js/app.js
+++ assets/css/js/app.js
@@ -23,11 +23,33 @@
   let day = days[now.getDay()];
 
   function displayForecast(){
-    let forecastElement = document.querySelector("#forecast");
+    let forecastElement= document.querySelector("#forecast");
+    
+    let forecastHTML=  `<div class="row">`;
+    days.forEach(function(day){
+      forecastHTML=
+      forecastHTML +
+        `
+        <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+      `;
+    });
 
-    forecastElement.innerHTML ="Forecast";
+      forecastHTML = forecastHTML + `</div>`;
+      forecastElement.innerHTML= forecastHTML;
+      console.log(forecastHTML);
   }
-
+    
    displayForecast();
 
   function updateDateInfo() {}
